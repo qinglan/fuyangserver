c48e92253d3d64ca7b7c2b3c87faba5d985ab4f3
heningwei <heningwei@126.com>
2018-10-01 14:50:15 +0800

    复制model

--- PictureText/admin.py
+++ PictureText/admin.py
@@ -2,7 +2,8 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import PictureTextColumn, PictureTextPaper
+from .models import PictureTextColumn, PictureTextColumn1, PictureTextColumn2, PictureTextColumn3, PictureTextColumn4, \
+    PictureTextPaper, PictureTextPaper1, PictureTextPaper2, PictureTextPaper3, PictureTextPaper4
 
 class PictureTextColumnAdmin(admin.ModelAdmin):
     list_display = ('name','id')
@@ -12,5 +13,13 @@ class PictureTextPaperAdmin(admin.ModelAdmin):
 
 
 admin.site.register(PictureTextPaper, PictureTextPaperAdmin)
+admin.site.register(PictureTextPaper1, PictureTextPaperAdmin)
+admin.site.register(PictureTextPaper2, PictureTextPaperAdmin)
+admin.site.register(PictureTextPaper3, PictureTextPaperAdmin)
+admin.site.register(PictureTextPaper4, PictureTextPaperAdmin)
 
 admin.site.register(PictureTextColumn, PictureTextColumnAdmin)
+admin.site.register(PictureTextColumn1, PictureTextColumnAdmin)
+admin.site.register(PictureTextColumn2, PictureTextColumnAdmin)
+admin.site.register(PictureTextColumn3, PictureTextColumnAdmin)
+admin.site.register(PictureTextColumn4, PictureTextColumnAdmin)
--- PictureText/models.py
+++ PictureText/models.py
@@ -19,6 +19,58 @@ class PictureTextColumn(models.Model):
         verbose_name_plural = '图文栏目'
         ordering = ['pk']
 
+class PictureTextColumn1(models.Model):
+    name = models.CharField('图文栏目名称', max_length=256)
+
+    introduce = models.TextField('栏目简介', default='')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = '图文栏目1'
+        verbose_name_plural = '图文栏目1'
+        ordering = ['pk']
+
+class PictureTextColumn2(models.Model):
+    name = models.CharField('图文栏目名称', max_length=256)
+
+    introduce = models.TextField('栏目简介', default='')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = '图文栏目2'
+        verbose_name_plural = '图文栏目2'
+        ordering = ['pk']
+
+class PictureTextColumn3(models.Model):
+    name = models.CharField('图文栏目名称', max_length=256)
+
+    introduce = models.TextField('栏目简介', default='')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = '图文栏目3'
+        verbose_name_plural = '图文栏目3'
+        ordering = ['pk']
+
+class PictureTextColumn4(models.Model):
+    name = models.CharField('图文栏目名称', max_length=256)
+
+    introduce = models.TextField('栏目简介', default='')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = '图文栏目4'
+        verbose_name_plural = '图文栏目4'
+        ordering = ['pk']
+
 
 class PictureTextPaper(models.Model):
     name = models.CharField('图文标题', max_length=256)
@@ -47,6 +99,114 @@ class PictureTextPaper(models.Model):
         verbose_name = '图文文章'
         verbose_name_plural = '图文文章'
 
+class PictureTextPaper1(models.Model):
+    name = models.CharField('图文标题', max_length=256)
+
+    column = models.ForeignKey(PictureTextColumn, on_delete=models.CASCADE, blank=True, verbose_name='所属栏目')
+
+    introduce = models.TextField('图文简介', default='')
+
+    image = models.ImageField('封面', upload_to='image')
+
+    register_date = models.DateTimeField('发表时间', default=timezone.now, editable=False)
+
+    content = UEditorField('图文内容', height=300, width=1000,
+                           default=u'', blank=True, imagePath="uploads/images/",
+                           toolbars='besttome', filePath='uploads/files/')
+
+    views_count = models.IntegerField('浏览次数', default=0, editable=False)
+
+    def __str__(self):
+        return self.name
+
+    def get_comment_count(self):
+        return len(PictureTextPaperComment.objects.filter(ascription=self))
+
+    class Meta:
+        verbose_name = '图文文章1'
+        verbose_name_plural = '图文文章1'
+
+class PictureTextPaper2(models.Model):
+    name = models.CharField('图文标题', max_length=256)
+
+    column = models.ForeignKey(PictureTextColumn, on_delete=models.CASCADE, blank=True, verbose_name='所属栏目')
+
+    introduce = models.TextField('图文简介', default='')
+
+    image = models.ImageField('封面', upload_to='image')
+
+    register_date = models.DateTimeField('发表时间', default=timezone.now, editable=False)
+
+    content = UEditorField('图文内容', height=300, width=1000,
+                           default=u'', blank=True, imagePath="uploads/images/",
+                           toolbars='besttome', filePath='uploads/files/')
+
+    views_count = models.IntegerField('浏览次数', default=0, editable=False)
+
+    def __str__(self):
+        return self.name
+
+    def get_comment_count(self):
+        return len(PictureTextPaperComment.objects.filter(ascription=self))
+
+    class Meta:
+        verbose_name = '图文文章2'
+        verbose_name_plural = '图文文章2'
+
+class PictureTextPaper3(models.Model):
+    name = models.CharField('图文标题', max_length=256)
+
+    column = models.ForeignKey(PictureTextColumn, on_delete=models.CASCADE, blank=True, verbose_name='所属栏目')
+
+    introduce = models.TextField('图文简介', default='')
+
+    image = models.ImageField('封面', upload_to='image')
+
+    register_date = models.DateTimeField('发表时间', default=timezone.now, editable=False)
+
+    content = UEditorField('图文内容', height=300, width=1000,
+                           default=u'', blank=True, imagePath="uploads/images/",
+                           toolbars='besttome', filePath='uploads/files/')
+
+    views_count = models.IntegerField('浏览次数', default=0, editable=False)
+
+    def __str__(self):
+        return self.name
+
+    def get_comment_count(self):
+        return len(PictureTextPaperComment.objects.filter(ascription=self))
+
+    class Meta:
+        verbose_name = '图文文章3'
+        verbose_name_plural = '图文文章3'
+
+class PictureTextPaper4(models.Model):
+    name = models.CharField('图文标题', max_length=256)
+
+    column = models.ForeignKey(PictureTextColumn, on_delete=models.CASCADE, blank=True, verbose_name='所属栏目')
+
+    introduce = models.TextField('图文简介', default='')
+
+    image = models.ImageField('封面', upload_to='image')
+
+    register_date = models.DateTimeField('发表时间', default=timezone.now, editable=False)
+
+    content = UEditorField('图文内容', height=300, width=1000,
+                           default=u'', blank=True, imagePath="uploads/images/",
+                           toolbars='besttome', filePath='uploads/files/')
+
+    views_count = models.IntegerField('浏览次数', default=0, editable=False)
+
+    def __str__(self):
+        return self.name
+
+    def get_comment_count(self):
+        return len(PictureTextPaperComment.objects.filter(ascription=self))
+
+    class Meta:
+        verbose_name = '图文文章4'
+        verbose_name_plural = '图文文章4'
+
 
 class PictureTextPaperComment(models.Model):
     message = models.CharField('图文评论', max_length=256)
@@ -60,4 +220,60 @@ class PictureTextPaperComment(models.Model):
     class Meta:
         verbose_name = '图文评论'
         verbose_name_plural = '图文评论'
+        ordering = ['-register_date']
+
+class PictureTextPaperComment1(models.Model):
+    message = models.CharField('图文评论', max_length=256)
+    ascription = models.ForeignKey(PictureTextPaper, on_delete=models.CASCADE, blank=True, verbose_name='所属图文')
+    register_date = models.DateTimeField('评论时间', auto_now_add=True, editable=False)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, verbose_name='作者')
+
+    def __str__(self):
+        return self.message
+
+    class Meta:
+        verbose_name = '图文评论1'
+        verbose_name_plural = '图文评论1'
+        ordering = ['-register_date']
+
+class PictureTextPaperComment2(models.Model):
+    message = models.CharField('图文评论', max_length=256)
+    ascription = models.ForeignKey(PictureTextPaper, on_delete=models.CASCADE, blank=True, verbose_name='所属图文')
+    register_date = models.DateTimeField('评论时间', auto_now_add=True, editable=False)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, verbose_name='作者')
+
+    def __str__(self):
+        return self.message
+
+    class Meta:
+        verbose_name = '图文评论2'
+        verbose_name_plural = '图文评论2'
+        ordering = ['-register_date']
+
+class PictureTextPaperComment3(models.Model):
+    message = models.CharField('图文评论', max_length=256)
+    ascription = models.ForeignKey(PictureTextPaper, on_delete=models.CASCADE, blank=True, verbose_name='所属图文')
+    register_date = models.DateTimeField('评论时间', auto_now_add=True, editable=False)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, verbose_name='作者')
+
+    def __str__(self):
+        return self.message
+
+    class Meta:
+        verbose_name = '图文评论3'
+        verbose_name_plural = '图文评论3'
+        ordering = ['-register_date']
+
+class PictureTextPaperComment4(models.Model):
+    message = models.CharField('图文评论', max_length=256)
+    ascription = models.ForeignKey(PictureTextPaper, on_delete=models.CASCADE, blank=True, verbose_name='所属图文')
+    register_date = models.DateTimeField('评论时间', auto_now_add=True, editable=False)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, verbose_name='作者')
+
+    def __str__(self):
+        return self.message
+
+    class Meta:
+        verbose_name = '图文评论4'
+        verbose_name_plural = '图文评论4'
         ordering = ['-register_date']
